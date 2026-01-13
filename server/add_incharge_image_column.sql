82c84f8bea127470e1fc4ce143289ec76a070a95
aadarsh1503 <aadarshchauhan35@gmail.com>
2026-01-13 18:26:34 +0530

    done

--- server/add_incharge_image_column.sql
+++
@@ -1,2 +0,0 @@
--- Add incharge_image column to users table
-ALTER TABLE `users` ADD COLUMN `incharge_image` varchar(500) DEFAULT NULL AFTER `logo`;
--- server/hashPassword.js
+++
@@ -1,8 +0,0 @@
-import bcrypt from 'bcryptjs';
-
-const plainPassword = '123456789';
-
-const salt = bcrypt.genSaltSync(10);
-const hashedPassword = bcrypt.hashSync(plainPassword, salt);
-
-console.log('Hashed Password:', hashedPassword);
